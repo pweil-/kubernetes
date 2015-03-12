6f0038ab042de1bfd521176fac87fcbc4d0d82be
Paul Weil <pweil@redhat.com>
2015-03-12 10:32:24 -0400

    auto-scaler types

--- pkg/api/v1beta2/register.go
+++ pkg/api/v1beta2/register.go
@@ -61,6 +61,8 @@ func init() {
 		&NamespaceList{},
 		&Secret{},
 		&SecretList{},
+		&AutoScaler{},
+		&AutoScalerList{},
 	)
 	// Future names are supported
 	api.Scheme.AddKnownTypeWithName("v1beta2", "Node", &Minion{})
@@ -96,3 +98,5 @@ func (*Namespace) IsAnAPIObject()                 {}
 func (*NamespaceList) IsAnAPIObject()             {}
 func (*Secret) IsAnAPIObject()                    {}
 func (*SecretList) IsAnAPIObject()                {}
+func (*AutoScaler) IsAnAPIObject()                {}
+func (*AutoScalerList) IsAnAPIObject()            {}
